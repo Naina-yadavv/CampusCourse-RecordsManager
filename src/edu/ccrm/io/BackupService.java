9c326d8f0c37956623ac1be0b80dfbc74f766b01
Naina-yadavv <nainayadav1410@gmail.com>
2025-09-09 21:22:21 +0530

    Initial skeleton - CCRM

---
+++ src/edu/ccrm/io/BackupService.java
@@ -0,0 +1,10 @@
+package edu.ccrm.io;
+
+import java.time.LocalDateTime;
+
+public class BackupService {
+    public void backupData(String sourcePath, String backupPath) {
+        String timestamp = LocalDateTime.now().toString().replace(":", "-");
+        System.out.println("Backing up data to " + backupPath + "/backup_" + timestamp);
+    }
+}
---
+++ src/edu/ccrm/util/Comparators.java
@@ -0,0 +1,8 @@
+package edu.ccrm.util;
+
+import edu.ccrm.domain.Course;
+import java.util.Comparator;
+
+public class Comparators {
+    public static Comparator<Course> byTitle = Comparator.comparing(Course::toString);
+}
